6d80cb037c5e5835f88e68e87e2bdd735efd5217
ShotaItasaka <shotachemistryt@gmail.com>
2023-07-28 17:00:24 +0000

    レイアウト調整

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_07_25_032118) do
+ActiveRecord::Schema.define(version: 2023_07_28_155355) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -54,6 +54,13 @@ ActiveRecord::Schema.define(version: 2023_07_25_032118) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  create_table "genres", force: :cascade do |t|
+    t.string "name", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["name"], name: "index_genres_on_name", unique: true
+  end
+
   create_table "post_comments", force: :cascade do |t|
     t.text "comment"
     t.integer "user_id"
@@ -68,6 +75,7 @@ ActiveRecord::Schema.define(version: 2023_07_25_032118) do
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
+    t.string "name"
     t.string "last_name", null: false
     t.string "first_name", null: false
     t.string "last_name_kana", null: false
